a6d6abc9c19dc28bad291930778a9e8da7415a07
Egor Zemlyakov <egor.zemlyakov@rt.ru>
2024-09-24 14:57:20 +0300

    update

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, g
-import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import inspect
-from sqlalchemy import text
+
 
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catalog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-
 
 db = SQLAlchemy(app)
 
@@ -22,14 +19,36 @@ class Product_Image(db.Model):
     url = db.Column(db.String(200), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products2.id'), nullable=False)
 
+
+class Metal_Breeds(db.Model):
+    __tablename__ = 'metal_breeds'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), unique=True, nullable=False)
+    translated = db.Column(db.String(200))
+
 class Products2(db.Model):
     __tablename__ = 'products2'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
-    metal = db.Column(db.Text)
+    metal_id = db.Column(db.Integer, db.ForeignKey('metal_breeds.id'), nullable=False)
+    metal = db.relationship('Metal_Breeds', backref=db.backref('product', lazy=True))
     type = db.Column(db.Text)
     images = db.relationship('Product_Image', backref='product', lazy=True)
 
+
+class Gems_Image(db.Model):
+    __tablename__ = 'gems_image'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(200), nullable=False)
+    gem_id = db.Column(db.Integer, db.ForeignKey('gems.id'), nullable=False)
+
+
+class Gems(db.Model):
+    __tablename__ = 'gems'
+    id = db.Column(db.Integer, primary_key=True)
+    breed = db.Column(db.Text)
+    category = db.Column(db.Text)
+    images = db.relationship('Gems_Image', backref='gem', lazy=True)
 
 
 
@@ -46,9 +65,6 @@ def catalog():
         inspector = inspect(db.engine)
         print("tables=", inspector.get_table_names())
         products = Products2.query.all()
-        print("products=",products)
-        for product in products:
-            print("prod=",product)
         return render_template('catalog.html', products=products)
 
 @app.route('/catalog/<int:product_id>')
@@ -68,9 +84,13 @@ def contact():
 def details():
     return render_template("details.html")
 
-@app.route("/catalog2")
-def catalog2():
-    return render_template("catalog2.html")
+@app.route("/catalog_gems")
+def catalog_gems():
+    with app.app_context():
+        inspector = inspect(db.engine)
+        print("tables=", inspector.get_table_names())
+        gems = Gems.query.all()
+        return render_template('catalog_gems.html', gems=gems)
 
 @app.route("/catalog3")
 def catalog3():
--- models.py
+++ models.py
@@ -1,17 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
-class Products(db.Model):
+
+class Product_Image(db.Model):
+    __tablename__ = 'product_image'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products2.id'), nullable=False)
+
+class Products2(db.Model):
+    __tablename__ = 'products2'
+    id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
-    metal = db.Column(db.String(50))
-    type = db.Column(db.String(50))
-    # images = db.relationship('ProductImage', backref='product', lazy=True)
+    metal = db.Column(db.Text)
+    type = db.Column(db.Text)
+    images = db.relationship('Product_Image', backref='product', lazy=True)
 
 
-# class ProductImage(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-#     image_path = db.Column(db.String(200), nullable=False)
+class Gems_Image(db.Model):
+    __tablename__ = 'gems_image'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(200), nullable=False)
+    gem_id = db.Column(db.Integer, db.ForeignKey('gems.id'), nullable=False)
+
+
+class Gems(db.Model):
+    __tablename__ = 'gems'
+    id = db.Column(db.Integer, primary_key=True)
+    breed = db.Column(db.Text)
+    category = db.Column(db.Text)
+    images = db.relationship('Gems_Image', backref='gem', lazy=True)
